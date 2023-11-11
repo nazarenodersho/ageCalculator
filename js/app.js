64bb20324b068b1d79a3e1c74681d5dcff96def2
NazarenoDersho <nazarenodersho@gmail.com>
2023-11-11 18:39:56 -0300

    Reset errors for calculator added

--- js/app.js
+++ js/app.js
@@ -62,14 +62,12 @@ document.getElementById('button').addEventListener('click', function () {
     if (day < 1 || day > 31) {
         error();
         document.getElementById('dayError').textContent = 'Must be a valid date';
-        return;
     }
 
     // Validate month range
     if (month < 1 || month > 12) {
         error();
         document.getElementById('monthError').textContent = 'Must be a valid month';
-        return;
     }
 
     // Get the current date again
@@ -84,80 +82,32 @@ document.getElementById('button').addEventListener('click', function () {
         return;
     }
 
+    //Reset errors before calculating if the user tries again with correct data
+    error(showError = false);
+
     calculateAge();
 });
 
 // Errors style function
-function error() {
+function error(showError = true) {
+
     const h2Element = document.querySelectorAll('.h2Error');
     h2Element.forEach(function(h2) {
-        h2.style.color = '#ff5757';
+        h2.style.color = showError ? '#ff5757': '';
     });
 
     const inputElement = document.querySelectorAll('.inputError');
     inputElement.forEach(function(input) {
-        input.style.border = '#ff5757 solid 1.5px';
+        input.style.border = showError ? '#ff5757 solid 1.5px': '';
     });
     
     const pElement = document.querySelectorAll('.pError');
     pElement.forEach(function(p) {
-        p.style.display = 'block';
-        p.style.color = '#ff5757';
-        p.style.fontWeight = '400';
-        p.style.fontStyle = 'italic';
-        p.style.fontSize = '0.5rem';
-        p.style.marginTop = '5px';
+        p.style.display = showError ? 'block' : 'none';
+        p.style.color = showError ? '#ff5757' : '';
+        p.style.fontWeight = showError ? '400' : '';
+        p.style.fontStyle = showError ? 'italic' : '';
+        p.style.fontSize = showError ? '0.5rem' : '';
+        p.style.marginTop = showError ? '5px': '';
     });
 };
-
-/*
-document.getElementById("submitButton").addEventListener("click", function() {
-    // Obtener los valores de los campos de fecha
-    const day = parseInt(document.getElementById("day").value);
-    const month = parseInt(document.getElementById("month").value);
-    const year = parseInt(document.getElementById("year").value);
-
-    // Validar si algún campo está vacío
-    if (!day || !month || !year) {
-        alert("Por favor, complete todos los campos.");
-        return;
-    }
-
-    // Validar el rango del día (1-31)
-    if (day < 1 || day > 31) {
-        alert("El número del día debe estar entre 1 y 31.");
-        return;
-    }
-
-    // Validar el rango del mes (1-12)
-    if (month < 1 || month > 12) {
-        alert("El número del mes debe estar entre 1 y 12.");
-        return;
-    }
-
-    // Obtener la fecha actual
-    const today = new Date();
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth() + 1; // Enero es el mes 0
-
-    // Validar si la fecha es en el futuro
-    if (year > currentYear || (year === currentYear && month > currentMonth) || (year === currentYear && month === currentMonth && day > today.getDate())) {
-        alert("La fecha de nacimiento no puede estar en el futuro.");
-        return;
-    }
-
-    // Validar la fecha inválida (por ejemplo, 31/04/1991)
-    if (day > daysInMonth(month, year)) {
-        alert("La fecha es inválida. El mes especificado no tiene suficientes días.");
-        return;
-    }
-
-    // Si no hay errores, puedes continuar con el cálculo de la edad y la presentación de los resultados
-    calculateAge();
-});
-
-function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-}
-
-*/
